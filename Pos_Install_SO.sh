04a20113a40fb5d4066e825d9ab38635daff154d
Emerson Barcelos <86887064+EmersonHBP@users.noreply.github.com>
2021-10-16 20:11:53 -0300

    Add files via upload

---
+++ Pos_Install_SO.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+## Removendo travas eventuais do apt ##
+
+sudo rm /var/lib/dpkg/lock-frontend &&
+sudo rm /var/cache/apt/archives/lock &&
+
+## Atualizando o repositório ##
+
+sudo apt update &&
+
+## Instalando pacotes e programas do repositório deb do ubuntu ##
+
+sudo apt install python3 python3-pip wine nautilus-dropbox docker docker-compose git build-essential libssl-dev flatpak gnome-software-plugin-flatpak winff -y &&
+
+
+## Download de programas externos ##
+
+mkdir /home/$USER/Downloads/programas &&
+
+cd /home/$USER/Downloads/programas &&
+
+## Google Chrome ##
+wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb &&
+
+## Simple Note ##
+wget -c https://github.com/Automattic/simplenote-electron/releases/download/v1.8.0/Simplenote-linux-1.8.0-amd64.deb &&
+
+## Team Viewer ##
+wget -c https://download.teamviewer.com/download/linux/teamviewer_amd64.deb &&
+
+sudo dpkg -i *.deb &&
+
+## Instalando Flatpak e adicionado repositório Flathub ##
+
+sudo apt install flatpak &&
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && 
+
+## Instalando programas em flatpak ##
+
+flatpak install flathub us.zoom.Zoom -y &&
+flatpak install flathub org.gimp.GIMP -y &&
+
+## Instalando Snap ##
+
+sudo apt install snapd &&
+
+## Instalando programas em snap ## 
+
+sudo snap install code --classic && 
+sudo snap install mysql-workbench-community &&
+
+# Instalando Atom ##
+
+sudo add-apt-repository ppa:webupd8team/atom &&
+sudo apt-get update &&
+sudo apt-get install atom &&
+
+## Instalando o Wine ## 
+
+sudo apt install wine &&
+
+## Finalização, atualização e limpeza##
+sudo apt update && sudo apt dist-upgrade -y
+flatpak update
+sudo apt autoclean
+sudo apt autoremove -y
+
